7d0aa21bd801c9f08f775e19344bc2d1351028df
Abhishek Gupta <92248080+Abhishek-1403@users.noreply.github.com>
2023-01-20 10:09:28 +0530

    Basic Structure Of App
    All Initial Structure in working Condition

---
+++ bootFirstWeb/src/main/java/com/springbootFirst/WebApp/BootFirstWebApplication.java
@@ -0,0 +1,13 @@
+package com.springbootFirst.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFirstWebApplication.class, args);
+	}
+
+}
---
+++ bootFirstWeb/src/test/java/com/springbootFirst/WebApp/BootFirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootFirst.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootFirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
